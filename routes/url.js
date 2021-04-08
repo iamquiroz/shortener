0d3433409ddebc1ee38a3510dfb04011d92f1336
J Quiroz <73316784+iamquiroz@users.noreply.github.com>
2021-04-08 10:12:17 -0800

    stage commit

---
+++ routes/url.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const validUrl = require("validUrl");
+const shortid = require("shortid");
+const config = require("config");
+
+const Url = require("../models/Url");
+
+// @route POST /api/url/shorten
+// @desc  Create short URL
+router.post("/shorten", (req, res) => {
+  const { longUrl } = req.body;
+  const baseUrl = config.get;
+
+  if (!validUrl.isUri(baseUrl)) {
+    return res.status(401).json("Invalid base url");
+  }
+});
+
+module.exports = router;
